e8a5f8b9c9fe90beb0b6aea5a73dac0d9e2ff595
Your Name <teakid21@gmail.com>
2021-06-17 12:48:26 +0200

    funcion generar elementos: por cada item, colores random, positiones y filas líena

--- innovation/main.js
+++ innovation/main.js
@@ -4,30 +4,34 @@ import {
 	OrbitControls
 } from "./node_modules/three/examples/jsm/controls/OrbitControls.js"
 
+let alturaSuelo = 0.49;
+let rowLength = 6;
+
+
 
 // ESCENA
 const scene = new THREE.Scene();
 scene.background = new THREE.Color("white");
-scene.fog = new THREE.Fog("white", 0.1, 20);
+scene.fog = new THREE.Fog("white", 0.1, 30);
 // CAMARAS
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-camera.position.set(5, 5, 5);
-
+camera.position.set(0, 2, 3);
+// camera.target = (0, 10, 1);
 
 
 // GEOMETRIA
-const geometryBox = new THREE.BoxGeometry(1, 1, 1);
-const materialBox = new THREE.MeshStandardMaterial({
-	color: 0xffffff
-});
-const box = new THREE.Mesh(geometryBox, materialBox);
+// const geometryBox = new THREE.BoxGeometry(1, 1, 1);
+// const materialBox = new THREE.MeshStandardMaterial({
+// 	color: 0xffffff
+// });
+// const box = new THREE.Mesh(geometryBox, materialBox);
 
-box.position.setY((box.geometry.parameters.height) / 2);
-box.castShadow = true;
-
-
-scene.add(box);
+// box.position.setY((box.geometry.parameters.height) / 2);
+// box.castShadow = true;
+//
+//
+// scene.add(box);
 
 
 
@@ -51,7 +55,7 @@ scene.add(hemisphereLight, pointLight);
 // GROUND
 const geometryGround = new THREE.PlaneGeometry(50, 50);
 const materialGround = new THREE.MeshStandardMaterial({
-	color: "blue"
+	color: "white"
 });
 
 const ground = new THREE.Mesh(geometryGround, materialGround);
@@ -65,9 +69,9 @@ scene.add(ground);
 
 
 // HELPERS
-// const gridHelper = new THREE.GridHelper(20, 20);
+const gridHelper = new THREE.GridHelper(20, 20);
 const pointLightHelper = new THREE.PointLightHelper(pointLight);
-scene.add(pointLightHelper);
+scene.add(pointLightHelper, gridHelper);
 
 
 
@@ -107,33 +111,108 @@ function animate() {
 
 }
 
-box.addEventListener("click", function() {
+renderer.domElement.addEventListener("click", function() {
 
 })
 
+const geometryBox = new THREE.BoxGeometry(1, 1, 1);
+const materialBox = new THREE.MeshStandardMaterial({
+	color: 0xffffff
+});
+const geometrySpehere = new THREE.SphereGeometry(0.5, 8, 6);
+const materialSpehere = new THREE.MeshStandardMaterial();
+
+
+const box01 = new THREE.Mesh(geometryBox, materialBox);
+const box02 = new THREE.Mesh(geometryBox, materialBox);
+const spehere03 = new THREE.Mesh(geometrySpehere, materialSpehere);
+const box04 = new THREE.Mesh(geometryBox, materialBox);
 
 
 
+let models = [box01, box02, spehere03, box04];
+
+
+let counterRow = 0;
 
 // FUNCTION
+let row = 0;
 
-// function generateElements(array) {
-// 	array.forEach((item, i) => {
-// 		let caja = new THREE.Mesh(geometryBox, materialBox);
-//
-//
-// 		caja.castShadow = true;
-// 		scene.add(caja);
-// 	});
-// }
-// // let boxes = [{
-// 	name: "caja1"
-// }, {
-// 	name: "caja2"
-// }];
+function generateElements(arrayModels, arrayPerson, colors) {
+	arrayPerson.forEach((item, i) => {
+		if (i % 5 == 0) {
+			row++;
+		}
+		let person = getRandomItem(arrayModels).clone();
+		let color = new THREE.MeshStandardMaterial();
+		person.material = color;
+		person.castShadow = true;
+		person.material.color.set(getRandomItem(colors));
+
+		person.position.set(setPosition(i, arrayPerson).x, setPosition(i, arrayPerson).y, setPosition(i, arrayPerson).z);
+
+		scene.add(person);
+	});
+}
+
+let boxes = [{
+		name: "caja1"
+	},
+	{
+		name: "caja2"
+	},
+	{
+		name: "caja3"
+	},
+	{
+		name: "caja4"
+	},
+	{
+		name: "caja5"
+	},
+	{
+		name: "caja6"
+	},
+	{
+		name: "caja7"
+	},
+	{
+		name: "caja8"
+	},
+	{
+		name: "caja9"
+	},
+	{
+		name: "caja10"
+	},
+	{
+		name: "caja11"
+	},
+	{
+		name: "caja12"
+	}
+];
+
+let colors = [0xc4ec6e, 0x7089fa, 0xef86f7, 0xb681eb];
+
+generateElements(models, boxes, colors);
+
+function getRandomItem(array) {
+	let index = Math.floor(Math.random() * array.length);
+	return array[index];
+}
 
 
+function setPosition(index, array) {
+	if (index === array.length - 1) {
+		const vector0 = new THREE.Vector3(0, alturaSuelo, 0);
+		return vector0;
+	}
 
+	let vector = new THREE.Vector3(index * 1.5 - (rowLength / 2) - ((rowLength + 1.5) * (row - 1)), alturaSuelo, -(6 + row * 2));
+	return vector;
+
+}
 
 
 
