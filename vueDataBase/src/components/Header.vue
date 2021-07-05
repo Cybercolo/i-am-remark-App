97e8e99a2f726caf9302601d3da892fd14f73374
lucas martinez <patrasioo69@gmail.com>
2021-07-05 13:22:49 +0200

    borreStyleHeader

--- vueDataBase/src/components/Header.vue
+++ vueDataBase/src/components/Header.vue
@@ -17,39 +17,4 @@ export default {
 
 <style  scoped>
 
-/*#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-* {
-  
-  box-sizing: border-box;
-  padding: 0px;
- 
-}*/
-
-header {
-  width: 100%;
-  max-width: 1024px;
-  display: flex;
-  justify-content: center; 
-}
-
-header img {
-  width: 100%;
-  max-width: 250px;
-  margin-top: 90px;
-}
-
-.main_pic_flecha_pg_01 {
-  Width: 55px;
-  Height: 45px;
-  margin-top: 18%;
-}
-
 </style>
