feac240444f552c7183f10889261a5e18bb93ad6
Your Name <teakid21@gmail.com>
2021-07-07 10:24:10 +0200

    Home page remaster

--- vueDataBase/src/views/Home.vue
+++ vueDataBase/src/views/Home.vue
@@ -1,41 +1,36 @@
 <template>
-  <div>
-    <body>
-      <div class="container">
-        <Header />
-        <div class="main_title_pg_01">
-          #IamRemarkable is a Google initiative  empowering women and other underrepresented groups to celebrate their achievements in the workplace  and beyond.
-        </div>
-        <div class="main_title_pg_01">
-          <p class="main_message_pg_01"> Tell to the World,</p>
-          <img class="main_pic_flecha_pg_01" src="../images/Vector_flecha.svg" alt="">
-        </div> 
-        <div class="main_button_why_you_pg_01">
-            <button class="button_pg_01" type="text" placeholder="Why are you Remarkable?" size="10">
-              <router-link to="/Formulario"> Why are you Remarkable? </router-link>
-            </button>
-        </div>
-        <div style= "margin-top:35px">
-          <img class="main_pic_allPeople_pg_01" src="../images/allPeople.png" alt="">
-        </div>
-      </div>
-    </body>
+<div class="containerlandpage">
+  <Header />
+  <div class="main_title_pg_01">
+    #IamRemarkable is a Google initiative empowering women and other
+    underrepresented groups to celebrate their achievements in the workplace
+    and beyond.
   </div>
+
+  <div class="main_text_pg_01">
+    <p class="main_message_pg_01">Tell to the World,</p>
+    <img class="main_pic_flecha_pg_01" src="../images/Vector_flecha.svg" alt="" />
+  </div>
+
+  <div class="main_llevame_03 main_button_why_you_pg_01">
+    <router-link to=" /Formulario">
+      Why are you remarkable?
+    </router-link>
+  </div>
+
+  <div class=" all">
+    <img class="main_pic_allPeople_pg_01" src="../images/allPeople.png" />
+  </div>
+</div>
 </template>
 
 <script>
+import Header from "@/components/Header.vue";
 
-import Header from '@/components/Header.vue'
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    Header,
-    
-  
+    Header
   }
-}
+};
 </script>
-
-<style scoped>
-
-</style>
