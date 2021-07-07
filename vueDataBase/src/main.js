3424d059761edef3f86ebd9c6a587d4309899ac1
Ceci <carbel.cecilia@gmail.com>
2021-07-07 10:29:17 +0200

    carpeta css

--- vueDataBase/src/main.js
+++ vueDataBase/src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import "@/assets/style.css"
-import "@/assets/pagina3.css"
-import "@/assets/pagina4.css"
+import "@/assets/css/style.css"
+import "@/assets/css/pagina3.css"
+import "@/assets/css/pagina4.css"
 
 createApp(App).use(store).use(router).mount('#app')
 
