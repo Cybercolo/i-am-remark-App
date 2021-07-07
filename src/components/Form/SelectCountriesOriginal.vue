f809029270369b2dee8cfcc37c1aff1578e3b80f
Your Name <teakid21@gmail.com>
2021-07-07 12:06:01 +0200

    el distaster

---
+++ src/components/Form/SelectCountriesOriginal.vue
@@ -0,0 +1,268 @@
+<template>
+    <div>
+        <select class="main-country-2" @click="dataLocal" v-model="country">
+            <option selected value= "CERO"> Country</option>
+            <option>Afganistán</option>
+            <option>Albania</option>
+            <option>Alemania</option>
+            <option>Andorra</option>
+            <option>Angola</option>
+            <option>Anguilla</option>
+            <option>Antártida</option>
+            <option>Antigua y Barbuda</option>
+            <option>Antillas Holandesas</option>
+            <option>Arabia Saudí</option>
+            <option>Argelia</option>
+            <option>Argentina</option>
+            <option>Armenia</option>
+            <option>Aruba</option>
+            <option>Australia</option>
+            <option>Austria</option>
+            <option>Azerbaiyán</option>
+            <option>Bahamas</option>
+            <option>Bahrein</option>
+            <option>Bangladesh</option>
+            <option>Barbados</option>
+            <option>Bélgica</option>
+            <option>Belice</option>
+            <option>Benin</option>
+            <option>Bermudas</option>
+            <option>Bielorrusia</option>
+            <option>Birmania</option>
+            <option>Bolivia</option>
+            <option>Bosnia y Herzegovina</option>
+            <option>Botswana</option>
+            <option>Brasil</option>
+            <option>Brunei</option>
+            <option>Bulgaria</option>
+            <option>Burkina Faso</option>
+            <option>Burundi</option>
+            <option>Bután</option>
+            <option>Cabo Verde</option>
+            <option>Camboya</option>
+            <option>Camerún</option>
+            <option>Canadá</option>
+            <option>Chad</option>
+            <option>Chile</option>  
+            <option>Chipre</option>
+            <option>Ciudad del Vaticano (Santa Sede)</option>
+            <option>Colombia</option>
+            <option>Comores</option>
+            <option>Congo</option>
+            <option>Congo, República Democrática del</option>
+            <option>Corea</option>
+            <option>Corea del Norte</option>
+            <option>Costa de Marfíl</option>
+            <option>Costa Rica</option>
+            <option>Croacia (Hrvatska)</option>
+            <option>Cuba</option>
+            <option>Dinamarca</option>
+            <option>Djibouti</option>
+            <option>Dominica</option>
+            <option>Ecuador</option>
+            <option>Egipto</option>
+            <option>El Salvador</option>
+            <option>Emiratos Árabes Unidos</option>
+            <option>Eritrea</option>
+            <option>Eslovenia</option>
+            <option>España</option>
+            <option>Estados Unidos</option>
+            <option>Estonia</option>
+            <option>Etiopía</option>
+            <option>Fiji</option>
+            <option>Filipinas</option>
+            <option>Finlandia</option>
+            <option>Francia</option>
+            <option>Gabón</option>
+            <option>Gambia</option>
+            <option>Georgia</option>
+            <option>Ghana</option>
+            <option>Gibraltar</option>
+            <option>Granada</option>
+            <option>Grecia</option>
+            <option>Groenlandia</option>
+            <option>Guadalupe</option>
+            <option>Guam</option>
+            <option>Guatemala</option>
+            <option>Guayana</option>
+            <option>Guayana Francesa</option>
+            <option>Guinea</option>
+            <option>Guinea Ecuatorial</option>
+            <option>Guinea-Bissau</option>
+            <option>Haití</option>
+            <option>Honduras</option>
+            <option>Hungría</option>
+            <option>India</option>
+            <option>Indonesia</option>
+            <option>Irak</option>
+            <option>Irán</option>
+            <option>Irlanda</option>
+            <option>Isla Bouvet</option>
+            <option>Isla de Christmas</option>
+            <option>Islandia</option>
+            <option>Islas Caimán</option>
+            <option>Islas Cook</option>
+            <option>Islas de Cocos o Keeling</option>
+            <option>Islas Faroe</option>
+            <option>Islas Heard y McDonald</option>
+            <option>Islas Malvinas</option>
+            <option>Islas Marianas del Norte</option>
+            <option>Islas Marshall</option>
+            <option>Islas menores de Estados Unidos</option>
+            <option>Islas Palau</option>
+            <option>Islas Salomón</option>
+            <option>Islas Svalbard y Jan Mayen</option>
+            <option>Islas Tokelau</option>
+            <option>Islas Turks y Caicos</option>
+            <option>Islas Vírgenes (EEUU)</option>
+            <option>Islas Vírgenes (Reino Unido)</option>
+            <option>Islas Wallis y Futuna</option>
+            <option>Israel</option>
+            <option>Italia</option>
+            <option>Jamaica</option>
+            <option>Japón</option>
+            <option>Jordania</option>
+            <option>Kazajistán</option>
+            <option>Kenia</option>
+            <option>Kirguizistán</option>
+            <option>Kiribati</option>
+            <option>Kuwait</option>
+            <option>Laos</option>
+            <option>Lesotho</option>
+            <option>Letonia</option>
+            <option>Líbano</option>
+            <option>Liberia</option>
+            <option>Libia</option>
+            <option>Liechtenstein</option>
+            <option>Lituania</option>
+            <option>Luxemburgo</option>
+            <option>Macedonia, Ex-República Yugoslava de</option>
+            <option>Madagascar</option>
+            <option>Malasia</option>
+            <option>Malawi</option>
+            <option>Maldivas</option>
+            <option>Malí</option>
+            <option>Malta</option>
+            <option>Marruecos</option>
+            <option>Martinica</option>
+            <option>Mauricio</option>
+            <option>Mauritania</option>
+            <option>Mayotte</option>
+            <option>México</option>
+            <option>Micronesia</option>
+            <option>Moldavia</option>
+            <option>Mónaco</option>
+            <option>Mongolia</option>
+            <option>Montserrat</option>
+            <option>Mozambique</option>
+            <option>Namibia</option>
+            <option>Nauru</option>
+            <option>Nepal</option>
+            <option>Nicaragua</option>
+            <option>Níger</option>
+            <option>Nigeria</option>
+            <option>Niue</option>
+            <option>Norfolk</option>
+            <option>Noruega</option>
+            <option>Nueva Caledonia</option>
+            <option>Nueva Zelanda</option>
+            <option>Omán</option>
+            <option>Países Bajos</option>
+            <option>Panamá</option>
+            <option>Papúa Nueva Guinea</option>
+            <option>Paquistán</option>
+            <option>Paraguay</option>
+            <option>Perú</option>
+            <option>Pitcairn</option>
+            <option>Polinesia Francesa</option>
+            <option>Polonia</option>
+            <option>Portugal</option>
+            <option>Puerto Rico</option>
+            <option>Qatar</option>
+            <option>Reino Unido</option>
+            <option>República Centroafricana</option>
+            <option>República Checa</option>
+            <option>República de Sudáfrica</option>
+            <option>República Dominicana</option>
+            <option>República Eslovaca</option>
+            <option>Reunión</option>
+            <option>Ruanda</option>
+            <option>Rumania</option>
+            <option>Rusia</option>
+            <option>Sahara Occidental</option>
+            <option>Saint Kitts y Nevis</option>
+            <option>Samoa</option>
+            <option>Samoa Americana</option>
+            <option>San Marino</option>
+            <option>San Vicente y Granadinas</option>
+            <option>Santa Helena</option>
+            <option>Santa Lucía</option>
+            <option>Santo Tomé y Príncipe</option>
+            <option>Senegal</option>
+            <option>Seychelles</option>
+            <option>Sierra Leona</option>
+            <option>Singapur</option>
+            <option>Siria</option>
+            <option>Somalia</option>
+            <option>Sri Lanka</option>
+            <option>St Pierre y Miquelon</option>
+            <option>Suazilandia</option>
+            <option>Sudán</option>
+            <option>Suecia</option>
+            <option>Suiza</option>
+            <option>Surinam</option>
+            <option>Tailandia</option>
+            <option>Taiwán</option>
+            <option>Tanzania</option>
+            <option>Tayikistán</option>
+            <option>Territorios franceses del Sur</option>
+            <option>Timor Oriental</option>
+            <option>Togo</option>
+            <option>Tonga</option>
+            <option>Trinidad y Tobago</option>
+            <option>Túnez</option>
+            <option>Turkmenistán</option>
+            <option>Turquía</option>
+            <option>Tuvalu</option>
+            <option>Ucrania</option>
+            <option>Uganda</option>
+            <option>Uruguay</option>
+            <option>Uzbekistán</option>
+            <option>Vanuatu</option>
+            <option>Venezuela</option>
+            <option>Vietnam</option>
+            <option>Yemen</option>
+            <option>Yugoslavia</option>
+            <option>Zambia</option>
+            <option>Zimbabue</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SelectCountries',
+    components:{
+
+    },
+    data() {
+        return {
+            country: ""
+        }
+    },
+    mounted() {
+        this.country = localStorage.country;
+    },
+    methods:{
+        dataLocal() {
+            localStorage.country = this.country;
+            console.log(localStorage, "pais")
+        }
+    }
+}
+   
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <header> 
+      <img class="header_logo" src="../images/brand.png" alt="">
+    </header>      
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Header'
+}
+
+</script>
+
+<style>
+
+</style>
