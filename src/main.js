b4777c06dcd28d93fb91c0dd921184f55e8666a8
thisisariann <ari.menendez13@gmail.com>
2021-07-08 12:31:33 +0200

    btnpg3

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -7,5 +8,6 @@ import "@/assets/css/style.css"
 import "@/assets/css/pagina3.css"
 import "@/assets/css/pagina4.css"
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app');
+
 
