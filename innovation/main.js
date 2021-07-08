0979a075ad64276f1a9ad73d24cc9c9d81ae03a9
Carlos Pinilla <carlos.pinilla.m@gmail.com>
2021-07-08 09:03:28 +0200

    fix touch

--- innovation/main.js
+++ innovation/main.js
@@ -43,10 +43,9 @@ let isPressedMe = false;
 
 //////////////////// ESCENA ////////////////////
 const scene = new Scene();
-const mainColor = 0x6FA8DC
-//scene.background = new Color(mainColor);
-
-scene.fog = new Fog(mainColor, 10, 25);
+const mainColor = 0xdde9ed
+scene.background = new Color(mainColor);
+scene.fog = new Fog( mainColor, 10, 25);
 
 //////////////////// CAMARAS ////////////////////
 const camera = new PerspectiveCamera(
@@ -76,10 +75,8 @@ pointLight.castShadow = true;
 scene.add(hemisphereLight, pointLight);
 //////////////////// GROUND ////////////////////
 const geometryGround = new PlaneGeometry(50, 50);
-
 const materialGround = new MeshStandardMaterial({
 	color: mainColor,
-	
 });
 
 const ground = new Mesh(geometryGround, materialGround);
@@ -101,12 +98,9 @@ const renderer = new WebGLRenderer({
 	alpha:true
 });
 
-
-
 renderer.shadowMap.enabled = true;
 
 renderer.render(scene, camera);
-
 
 //////////////////// FUNCTIONS ////////////////////
 
@@ -193,17 +187,9 @@ function generateElements(arrayPerson, colors) {
 
 // SCROLL
 let wheelCount = camera.position.z;
-
-camera.position.z = 4;
-
-console.log(camera.position);
-let maxZoomOutValue = 7;
-
-/**
 canvas.addEventListener("wheel", function(e) {
-	longerGround(e);
+	let maxZoomOutValue = 7;
 	let cameraTargetVector = getCameraTargetVector();
-	
 	camera.lookAt(cameraTargetVector);
 
 	pointLight.position.set(
@@ -212,10 +198,6 @@ canvas.addEventListener("wheel", function(e) {
 		camera.position.z + 10
 	); // pointlight following camera
 	camera.position.z = getWheelCount(e);
-	
-	pointLight.position.set(pointLight.position.x, pointLight.position.y, camera.position.z + 10); // pointlight following camera
-	
-	
 
 	if (camera.position.z > maxZoomOutValue) {
 		// camera zoom out limit
@@ -241,12 +223,12 @@ function moveCameraYScroll(e) {
 	}
 }
 
-
-
-
+function getCameraTargetVector() {
+	let cameraTargetVector = new Vector3(0, 1, camera.position.z - 15);
+	return cameraTargetVector;
+}
 
 function getWheelCount(e) {
-	console.log(e.deltaY);
 	e.deltaY < 0 ? wheelCount-- : wheelCount++;
 	return wheelCount;
 }
@@ -264,60 +246,6 @@ function stopZoomOut(maxZoomOutValue) {
 	wheelCount = maxZoomOutValue;
 	groundLength = 1;
 }
-
- */
-let cameraPositionCounter = 0;
-
-let positionFirst = {x: 0, y: 1, z: 4};
-let positionRow = {x: 0, y: 5, z: -7};
-let duration = 500;
-
-
-
-
-function getCameraTargetVector() {
-	let cameraTargetVector = new Vector3(0, 1, camera.position.z - 15);
-	return cameraTargetVector;
-}
-
-
-function getTargetPositionCamara(cameraPositionCounter) {
-	let targetPosition = new Vector3(0, 5, -((cameraPositionCounter * distanceBetweenRows)));
-	console.log(targetPosition)
-	return targetPosition;
-}
-
-
-
-
-
-
-function moveFowards(){
-
-	cameraPositionCounter++;
-	positionRow.z = ( cameraPositionCounter * distanceBetweenRows);
-	let targetPosition = positionRow;
-
-	tweenCube(targetPosition, duration);
-}
-
-function moveBackwards(){
-	if (cameraPositionCounter === 0){
-		return
-	}
-	cameraPositionCounter--;
-	let targetPosition = getTargetPositionCamara(cameraPositionCounter)
-	let duration = 500;
-	tweenCube(targetPosition, duration, camera);
-}
-
-
-
-
-document.querySelector(".forwards").addEventListener("click", moveFowards);
-document.querySelector(".backwards").addEventListener("click", moveBackwards);
-
-/*
 
 function getTargetPositionCamara(cameraPositionCounter, isFront) {
 	let targetPosition;
@@ -377,12 +305,12 @@ document.querySelector(".backwards").addEventListener("click", function() {
 		}
 	}
 });
-*/
+
 function tweenCube(targetPosition, duration) {
 	let currentPosition = camera.position;
 	let tween = new TWEEN.Tween(currentPosition)
-	tween.to(targetPosition, duration)
-	tween.onUpdate(function() {
+		.to(targetPosition, duration)
+		.onUpdate(function() {
 			camera.position.y = currentPosition.y;
 			pointLight.position.set(
 				pointLight.position.x,
@@ -392,7 +320,7 @@ function tweenCube(targetPosition, duration) {
 			camera.lookAt(getCameraTargetVector());
 			ground.scale.y = 1 + cameraPositionCounter / 1.5;
 		})
-	tween.start();
+		.start();
 }
 
 function generateBubbles() {
@@ -407,32 +335,6 @@ function generateBubbles() {
 	});
 }
 
-document.addEventListener("touchstart", getYPosInit);
-document.addEventListener("touchmove", getYPosEnd);
-document.addEventListener("touchend", getYDirection);
-let posYInit;
-let posYEnd;
-
-
-
-function getYPosInit(e){
-	posYInit = e.touches[0].clientY;
-}
-
-function getYPosEnd(e){
-	posYEnd = e.touches[0].clientY;
-}
-
-function getYDirection(e){
-	if (posYInit < posYEnd){
-		moveFowards()
-	}
-	if (posYEnd - posYInit < 100){
-		moveBackwards()	
-		}
-	}
-
-	
 function hideBubbles() {
 	bubblesContainer.style.transform = `translateX(0)`;
 	bubblesContainer.style.display = "none";
@@ -465,6 +367,71 @@ function animate() {
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.render(scene, camera);
 }
+
+
+//// ADDitionals ///
+
+let positionFirst = {x: 0, y: 1, z: 4};
+let positionRow = {x: 0, y: 5, z: -7};
+let duration = 500;
+
+
+function moveFowards(){
+	cameraPositionCounter++;
+	let targetPosition = getTargetPositionCamara(cameraPositionCounter, true);
+	tweenCube(targetPosition, duration, cameraPositionCounter);
+		
+
+
+	
+}
+
+function moveBackwards(){
+	if (cameraPositionCounter === 0){
+		return
+	}
+	cameraPositionCounter--;
+	let targetPosition = getTargetPositionCamara(cameraPositionCounter, false);
+	tweenCube(targetPosition, duration);
+}
+
+/*
+
+
+document.querySelector(".forwards").addEventListener("click", moveFowards);
+document.querySelector(".backwards").addEventListener("click", moveBackwards)
+
+*/
+document.addEventListener("touchstart", getYPosInit);
+document.addEventListener("touchmove", getYPosEnd);
+document.addEventListener("touchend", getYDirection);
+let posYInit;
+let posYEnd;
+
+
+
+function getYPosInit(e){
+	posYInit = e.touches[0].clientY;
+}
+
+function getYPosEnd(e){
+	posYEnd = e.touches[0].clientY;
+}
+
+function getYDirection(e){
+	if (posYInit < posYEnd){
+		moveFowards()
+	}
+	if (posYEnd - posYInit < 100){
+		moveBackwards()	
+		}
+	
+}
+
+
+//// En Add ///
+
+
 
 animate();
 loadNextModel();
